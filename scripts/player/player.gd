0fd607ab6184dd6ce94635f7fbdf4d5c83368537
FvlSky <mariahannavb@gmail.com>
2026-05-28 12:51:35 +0800

    feat: Setup main map instantiation, camera framing, and player movement

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,11 +1,17 @@
-extends Node
+extends CharacterBody2D
 
+@export var speed: float = 200.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	# Now listening for your custom WASD actions
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	# 1. Check if the keys are being read
+	print("Direction: ", direction) 
+	
+	velocity = direction * speed
+	
+	# 2. Check if the speed is actually being applied
+	print("Velocity: ", velocity)
+	
+	move_and_slide()
