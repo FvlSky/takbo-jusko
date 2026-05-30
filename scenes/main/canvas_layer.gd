28ec135bd059217b3a4501ac042c5d508eb75127
Julianna Leila Castillo <juliannaleilacastillo@gmail.com>
2026-05-30 13:33:05 +0800

    feat: imported timer assets

--- scenes/main/canvas_layer.gd
+++ scenes/main/canvas_layer.gd
@@ -10,13 +10,14 @@ func _process(_delta):
 		var time_left = timer.time_left
 		var minutes = int(time_left / 60.0)
 		var seconds = int(time_left) % 60
-		label.text = "%02d:%02d" % [minutes, seconds]
+		var milliseconds = int(fmod(time_left, 1.0) * 100)
+		
+		label.text = "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
 
 func _on_button_pressed():
 	timer.start()
-	if button:
-		button.hide()
-
+		
+		
 func _on_timer_timeout():
-	label.text = "00:00"
+	label.text = "00:00:00"
 	print("Time is up!")
