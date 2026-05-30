2cc88c386a6076000a2f00aec19451a141bd22e3
Julianna Leila Castillo <juliannaleilacastillo@gmail.com>
2026-05-30 14:03:35 +0800

    feat: temporary menu screen

--- scenes/main/canvas_layer.gd
+++ scenes/main/canvas_layer.gd
@@ -2,8 +2,9 @@ extends CanvasLayer
 
 @onready var timer = $Timer
 @onready var label = $Label
-@onready var button = $PlayButton
-@onready var title = $Title
+
+func start_game():
+	timer.start()
 
 func _process(_delta):
 	if timer.time_left > 0:
@@ -11,13 +12,8 @@ func _process(_delta):
 		var minutes = int(time_left / 60.0)
 		var seconds = int(time_left) % 60
 		var milliseconds = int(fmod(time_left, 1.0) * 100)
-		
 		label.text = "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
 
-func _on_button_pressed():
-	timer.start()
-		
-		
 func _on_timer_timeout():
 	label.text = "00:00:00"
 	print("Time is up!")
