f54ea92675a4a376a27657bfc417a81ef4c5a0e8
Julianna Leila Castillo <juliannaleilacastillo@gmail.com>
2026-05-30 13:12:28 +0800

    feat: test play button

--- scenes/main/canvas_layer.gd
+++ scenes/main/canvas_layer.gd
@@ -2,13 +2,20 @@ extends CanvasLayer
 
 @onready var timer = $Timer
 @onready var label = $Label
+@onready var button = $PlayButton
+@onready var title = $Title
 
-func _process(delta):
+func _process(_delta):
 	if timer.time_left > 0:
 		var time_left = timer.time_left
 		var minutes = int(time_left / 60.0)
 		var seconds = int(time_left) % 60
 		label.text = "%02d:%02d" % [minutes, seconds]
+
+func _on_button_pressed():
+	timer.start()
+	if button:
+		button.hide()
 
 func _on_timer_timeout():
 	label.text = "00:00"
